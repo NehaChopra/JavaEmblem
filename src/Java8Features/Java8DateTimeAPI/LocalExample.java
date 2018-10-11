2ba902bfc243d5803335a040742ddaef1fb0ca09
neha chopra <neha.chopra@paytm.com>
2018-10-11 11:23:00 +0530

    java features

--- src/Java8Features/Java8DateTimeAPI/LocalExample.java
+++ src/Java8Features/Java8DateTimeAPI/LocalExample.java
@@ -4,7 +4,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public class Example {
+public class LocalExample {
 
 	public static void main(String[] args) {
 		
